41ec0bef994f587a9177da8a6153a2cbea113582
erina-ameen <erina.ameen@gmail.com>
2024-09-10 15:29:15 +0100

    Add files via upload

--- Connect the Fruits/ConnecttheFruits.py
+++ Connect the Fruits/ConnecttheFruits.py
@@ -1,12 +1,14 @@
 import pgzrun, random, time
-WIDTH=893
-HEIGHT=500
+WIDTH=960
+HEIGHT=768
 start=0
-time=0
+time=10
 end=0
 fruits=[]
 lines=[]
 next=0
+no=5
+game=False
 
 def create():
     global start
@@ -16,13 +18,27 @@ def create():
     pineapple=Actor("pineapple.png")
     strawberry=Actor("strawberry.png")
     fruits.extend([blueberry,cherry,kiwi,pineapple,strawberry])
-    for i in fruits:
-        i.pos=random.randint(0,WIDTH),random.randint(0,HEIGHT)
     #start=time.time()
 
 def draw():
-    for i in fruits:
-        i.draw()
+    screen.clear()
+    screen.blit("forest.jpeg",(0,0))
+    var=1
+    for j in fruits:
+        j.pos=random.randint(20,WIDTH-20),random.randint(20,HEIGHT-20)
+        j.draw() 
+        screen.draw.text(str(var),(j.pos[0],j.pos[1]+5),fontsize=50)
+        var=var+1
+    for y in lines:
+        screen.draw.line(y[0],y[1],(56,123,189))
+        
+def on_mouse_down(pos): 
+    global lines, next
+    if next<no:
+        if fruits[next].collidepoint(pos):
+            if next:
+                lines.append((fruits[next-1].pos,fruits[next].pos))
+            next=next+1
 create()
 
 pgzrun.go()
