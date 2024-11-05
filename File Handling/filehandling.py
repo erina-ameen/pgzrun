d03f43cc9d057b7b8d67b492fef1470d972f9899
erina-ameen <erina.ameen@gmail.com>
2024-11-05 15:27:42 +0000

    filehandling.py

--- File Handling/filehandling.py
+++ File Handling/filehandling.py
@@ -1,20 +1,53 @@
-#Opening File
-file=open("file1.txt","r")
-for i in file:
-    print(i)
+import pgzrun
+TITLE="Pop Quiz"
+WIDTH=900
+HEIGHT=820
 
-#Method 2
-file=open("file1.txt","r")
-#print(file.read())
-print(file.read(3))
+#Box Definition
+scroll=Rect(0,0,900,200)
+scroll.move_ip(0,0)
+quest=Rect(0,0,600,180)
+quest.move_ip(20,220)
+timer=Rect(0,0,250,180)
+timer.move_ip(630,220)
+skip=Rect(0,0,250,360)
+skip.move_ip(630,420)
 
-#Writing a File
-file=open("file1.txt","w")
-file.write("what is your name?\n")
-file.write("where are you from?\n")
-file.close()
+opt1=Rect(0,0,270,180)
+opt1.move_ip(20,420)
+opt2=Rect(0,0,270,180)
+opt2.move_ip(325,420)
+opt3=Rect(0,0,270,180)
+opt3.move_ip(20,615)
+opt4=Rect(0,0,270,180)
+opt4.move_ip(325,615)
 
-#Append File
-file=open("file1.txt","a")
-file.write("my name is erina")
-file.close()
+#Game Variables
+seconds=15
+score=0
+message=""
+gameover=False
+question="questions.txt"
+opts=[opt1,opt2,opt3,opt4]
+qlist=[]
+qnumber=0
+index=0
+
+def draw():
+    global message
+    screen.fill((255,255,255))
+    screen.draw.filled_rect(scroll,(229,204,255))
+    screen.draw.filled_rect(quest,"light blue")
+    screen.draw.filled_rect(timer,"light yellow")
+    screen.draw.filled_rect(skip,"light green")
+    screen.draw.filled_rect(opt1,"light pink")
+    screen.draw.filled_rect(opt2,"light pink")
+    screen.draw.filled_rect(opt3,"light pink")
+    screen.draw.filled_rect(opt4,"light pink")
+    message="Welcome to Pop Quiz"
+    screen.draw.textbox(message,scroll,color="black")
+
+def update():
+    pass
+
+pgzrun.go()
