3a85e88e68b0b1b76eb1b5e6bb76e5689f5f33fc
erina-ameen <erina.ameen@gmail.com>
2024-12-17 15:07:41 +0000

    catchthefly.py

--- Catch the Fly/catchthefly.py
+++ Catch the Fly/catchthefly.py
@@ -2,20 +2,26 @@ import pgzrun, random
 WIDTH=1200
 HEIGHT=600
 
-fly=Actor("fly.png")
 spider=Actor("spider.png")
-fly.pos=600,80
 spider.pos=600,500
 webs=[]
 end=False
 score=0
 count=10
+enemies=[]
+
+for i in range(8):
+    fly=Actor("fly.png")
+    enemies.append(fly)
+    enemies[-1].x=75+85*i 
+    enemies[-1].y=0
 
 def draw():
     global webs,score
-    screen.fill((229, 204, 255))
+    screen.fill((34, 78, 255))
     spider.draw()
-    fly.draw()
+    for i in enemies:
+        i.draw()
     screen.draw.text("Score:"+str(score),center=(70,10),fontsize=35)
     for i in webs:
         i.draw()
@@ -26,19 +32,25 @@ def draw():
 def on_key_down(key):
     global webs
     if key==keys.SPACE:
-        webs.append(Actor("flash.png"))
+        webs.append(Actor("web.png"))
         webs[-1].x=spider.x
         webs[-1].y=spider.y-50      
 
+direction=1
+
 def update():
-    global webs, score, count
+    global webs, score, count, direction
     pass
-    fly.y+=5.5
-    if fly.y>=600:
-        fly.y=0
-        fly.x=random.randint(0,1200)
-        score-=10
-        count-=1
+    move=False
+    if len(enemies)>0 and (enemies[-1].x>WIDTH-80 or enemies[0].x<80):
+        direction=direction*-1
+        move=True
+    for i in enemies:
+        i.x+=3*direction
+        if move==True:
+            i.y+=20
+        #score-=10
+        #count-=1
         if count==0:
             gameover()
 
@@ -54,7 +66,7 @@ def update():
 
     for i in webs:
         i.y-=10
-
+         
     for bullet in webs :
         if fly.colliderect(bullet):
             fly.y=0
